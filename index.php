74e0ce463a10aa3ac63d3b576531cd87e30d196a
Baek Byoeng han <bhbaek@Baeks-MacBook-Air.local>
2014-07-26 16:25:06 +0900

    test

--- index.php
+++ index.php
@@ -3,49 +3,62 @@
 require_once('autoload.php');
 
 
+
+
+session_start();
+require_once( 'Facebook/FacebookSession.php' );
+require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
+require_once( 'Facebook/FacebookRequest.php' );
+require_once( 'Facebook/FacebookResponse.php' );
+require_once( 'Facebook/FacebookSDKException.php' );
+require_once( 'Facebook/FacebookRequestException.php' );
+require_once( 'Facebook/FacebookAuthorizationException.php' );
+require_once( 'Facebook/GraphObject.php' );
+
 use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
-use Facebook\GraphUser;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
 use Facebook\FacebookRequestException;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
 
-
+// init app with app id (APPID) and secret (SECRET)
 FacebookSession::setDefaultApplication('680605312014481','8f7df2bbfa7259bafe6ec9443f054776');
 
-$helper = new FacebookCanvasLoginHelper();
+// login helper with redirect_uri
+$helper = new FacebookRedirectLoginHelper( 'http://www.metah.ch/' );
+
 try {
-  $session = $helper->getSession();
-} catch (FacebookRequestException $ex) {
-    // When Facebook returns an error
-} catch (\Exception $ex) {
-    // When validation fails or other local issues  
+  $session = $helper->getSessionFromRedirect();
+} catch( FacebookRequestException $ex ) {
+  // When Facebook returns an error
+} catch( Exception $ex ) {
+  // When validation fails or other local issues
 }
-if ($session) {
-  echo 1;
-  // Logged in.
 
-    try {
+// see if we have a session
+if ( isset( $session ) ) {
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/me' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject();
 
-    $user_profile = (new FacebookRequest(
-      $session, 'GET', '/me'
-    ))->execute()->getGraphObject(GraphUser::className());
-
-    echo "Name: " . $user_profile->getName();
-
-  } catch(FacebookRequestException $e) {
-
-    echo "Exception occured, code: " . $e->getCode();
-    echo " with message: " . $e->getMessage();
-
-  }   
-
+  // print data
+  echo  print_r( $graphObject, 1 );
+} else {
+  // show login url
+  echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
 }
 
 
 
-}
+
+
+
+
 
 
 ?>
