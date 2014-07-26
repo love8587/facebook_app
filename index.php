68411c063bb6b5bded02fde818d5bb7b8aeb3422
Baek Byoeng han <bhbaek@Baeks-MacBook-Air.local>
2014-07-26 19:21:30 +0900

    sd

--- index.php
+++ index.php
@@ -4,56 +4,76 @@ require_once('autoload.php');
 
 
 
-// Must pass session data for the library to work
-session_start();
-
 
  
-use Facebook\GraphSessionInfo;
+use Facebook\HttpClients\FacebookCurl;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+ 
+use Facebook\Entities\AccessToken;
+use Facebook\Entities\SignedRequest;
+ 
 use Facebook\FacebookSession;
-use Facebook\FacebookCurl;
-use Facebook\FacebookHttpable;
-use Facebook\FacebookCurlHttpClient;
-use Facebook\FacebookResponse;
-use Facebook\FacebookAuthorizationException;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookRequest;
-use Facebook\FacebookSDKException;
 use Facebook\Helpers\FacebookRedirectLoginHelper;
+use Facebook\FacebookSignedRequestFromInputHelper; // added in v4.0.9
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookOtherException;
+use Facebook\FacebookAuthorizationException;
 use Facebook\GraphObject;
-
+use Facebook\GraphSessionInfo;
  
-// Replace the APP_ID and APP_SECRET with your apps credentials
-//FacebookSession::setDefaultApplication( 'APP_ID', 'APP_SECRET' );
+// these two classes required for canvas and tab apps
+use Facebook\Helpers\FacebookCanvasLoginHelper;
+use Facebook\Helpers\FacebookPageTabHelper;
+ 
+// start session
+session_start();
+ 
+// init app with app id and secret
+//FacebookSession::setDefaultApplication( 'xxx','yyy' );
 FacebookSession::setDefaultApplication('680605312014481','8f7df2bbfa7259bafe6ec9443f054776');
-
-
-// login helper with redirect_uri
-$helper = new FacebookRedirectLoginHelper( 'http://boiling-caverns-1628.herokuapp.com/' );
+// init login helper
+$helper = new FacebookRedirectLoginHelper( 'http://boiling-caverns-1628.herokuapp.com/index.php' );
  
-try {
-  $session = $helper->getSessionFromRedirect();
-} catch( FacebookRequestException $ex ) {
-  // When Facebook returns an error
-} catch( Exception $ex ) {
-  // When validation fails or other local issues
-}
+// init page tab helper
+$pageHelper = new FacebookPageTabHelper();
+ 
+// get session from the page
+$session = $pageHelper->getSession();
+ 
+// get page_id
+echo '<p>You are currently viewing page: '. $pageHelper->getPageId() . '</p>';
+ 
+// get like status - use for likegates
+echo '<p>You have '. ( $pageHelper->isLiked() ? 'LIKED' : 'NOT liked' ) . ' this page</p>';
+ 
+// get admin status
+echo '<p>You are '. ( $pageHelper->isAdmin() ? 'an ADMIN' : 'NOT an ADMIN' ) . '</p>';
  
 // see if we have a session
 if ( isset( $session ) ) {
+  
+  // show logged-in user id
+  echo 'User Id: ' . $pageHelper->getUserId();
+  
   // graph api request for user data
   $request = new FacebookRequest( $session, 'GET', '/me' );
   $response = $request->execute();
   // get response
-  $graphObject = $response->getGraphObject();
-   
-  // print data
-  echo  print_r( $graphObject, 1 );
+  $graphObject = $response->getGraphObject()->asArray();
+  
+  // print profile data
+  echo '<pre>' . print_r( $graphObject, 1 ) . '</pre>';
+  
+  // print logout url, target = _top to break out of page frame
+  echo '<a href="' . $helper->getLogoutUrl( $session, 'http://boiling-caverns-1628.herokuapp.com' ) . '" target="_top">Logout</a>';
+  
 } else {
-  // show login url
-  echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
+  // show login url, target = _top to break out of page frame
+  echo '<a href="' . $helper->getLoginUrl( array( 'email', 'user_friends' ) ) . '" target="_top">Login</a>';
 }
-
 
 
 ?>
