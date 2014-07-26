578cec9de29256e60534708653c86371cc254049
Baek Byoeng han <bhbaek@Baeks-MacBook-Air.local>
2014-07-27 01:41:36 +0900

    as

--- index.php
+++ index.php
@@ -55,13 +55,55 @@ var_dump($test3);
 <body>
 <div id="fb-root"></div>
 <script>
-   window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '518851781580229',
-          xfbml      : true,
-          version    : 'v2.0'
-        });
-      };
+
+  // This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
+
+
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+
+ window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '518851781580229',
+        xfbml      : true,
+        version    : 'v2.0'
+      });
+    };
+
+
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  };
+
+
 
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -71,16 +113,16 @@ var_dump($test3);
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-
-FB.login(
-  function(response) {
-    console.log(response);
-  },
-  {
-    scope: 'user_likes',
-    auth_type: 'rerequest'
-  }
-);
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
+}
 
 
 </script>
@@ -97,13 +139,13 @@ FB.login(
 
 $session = $helper->getSession();
 
-
-  /* make the API call */
+/* make the API call */
 $request = new FacebookRequest(
   $session,
   'GET',
   '/me'
 );
+
 $response = $request->execute();
 $graphObject = $response->getGraphObject();
 /* handle the result */
@@ -113,6 +155,14 @@ print_r($graphObject);
 echo "</pre>";
 
 } ?>
+
+
+<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+</fb:login-button>
+
+<div id="status">
+</div>
+
 
 </body>
 </html>
