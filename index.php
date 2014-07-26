a60532e1cef6fdbd72924560b937775a6f1dd0be
Baek Byoeng han <bhbaek@Baeks-MacBook-Air.local>
2014-07-26 16:09:30 +0900

    test

--- index.php
+++ index.php
@@ -10,20 +10,17 @@ use Facebook\FacebookRequestException;
 
 FacebookSession::setDefaultApplication('680605312014481','8f7df2bbfa7259bafe6ec9443f054776');
 
-$helper = new FacebookRedirectLoginHelper('https://apps.facebook.com/byeonghan/');
-$loginUrl = $helper->getLoginUrl();
-// Use the login url on a link or button to redirect to Facebook for authentication
-
+$helper = new FacebookCanvasLoginHelper();
 try {
-  $session = $helper->getSessionFromRedirect();
-} catch(FacebookRequestException $ex) {
-  // When Facebook returns an error
-} catch(\Exception $ex) {
-  // When validation fails or other local issues
+  $session = $helper->getSession();
+} catch (FacebookRequestException $ex) {
+    // When Facebook returns an error
+} catch (\Exception $ex) {
+    // When validation fails or other local issues  
 }
 if ($session) {
-  // Logged in
   echo 1;
+  // Logged in.
 }
 
 
