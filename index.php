80be6f85a2b60059c7c7c0fa526b236fb790a415
Baek Byoeng han <bhbaek@Baeks-MacBook-Air.local>
2014-07-26 18:30:23 +0900

    a

--- index.php
+++ index.php
@@ -4,46 +4,102 @@ require_once('autoload.php');
 
 
 
-
+// Must pass session data for the library to work
 session_start();
 
 
-
-
+ 
+use Facebook\GraphSessionInfo;
 use Facebook\FacebookSession;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
+use Facebook\FacebookCurl;
+use Facebook\FacebookHttpable;
+use Facebook\FacebookCurlHttpClient;
+use Facebook\FacebookResponse;
+use Facebook\FacebookAuthorizationException;
 use Facebook\FacebookRequestException;
-use Facebook\Helpers\FacebookCanvasLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookSDKException;
+use Facebook\Helpers\FacebookRedirectLoginHelper;
+use Facebook\GraphObject;
 
+ 
+// Replace the APP_ID and APP_SECRET with your apps credentials
+//FacebookSession::setDefaultApplication( 'APP_ID', 'APP_SECRET' );
 FacebookSession::setDefaultApplication('680605312014481','8f7df2bbfa7259bafe6ec9443f054776');
 
-// Use one of the helper classes to get a FacebookSession object.
-//   FacebookRedirectLoginHelper
-//   FacebookCanvasLoginHelper
-//   FacebookJavaScriptLoginHelper
-// or create a FacebookSession with a valid access token:
-
-
-
-$helper = new FacebookCanvasLoginHelper();
-try {
-  $session = $helper->getSession();
-
-var_dump($session);
-
-} catch(FacebookRequestException $ex) {
-  // When Facebook returns an error
-} catch(\Exception $ex) {
-  // When validation fails or other local issues
+// Create the login helper and replace REDIRECT_URI with your URL
+// Use the same domain you set for the apps 'App Domains'
+// e.g. $helper = new FacebookRedirectLoginHelper( 'http://mydomain.com/redirect' );
+$helper = new FacebookRedirectLoginHelper( 'https://boiling-caverns-1628.herokuapp.com/' );
+ 
+// Check if existing session exists
+if ( isset( $_SESSION ) && isset( $_SESSION['fb_token'] ) ) {
+ 
+  // Create new session from saved access_token
+  $session = new FacebookSession( $_SESSION['fb_token'] );
+ 
+  // Validate the access_token to make sure it's still valid
+  try {
+    if ( ! $session->validate() ) {
+      $session = null;
+    }
+  } catch ( Exception $e ) {
+    // Catch any exceptions
+    $session = null;
+  }
+} else {
+ 
+  // No session exists
+  try {
+    $session = $helper->getSessionFromRedirect();
+  } catch( FacebookRequestException $ex ) {
+ 
+    // When Facebook returns an error
+  } catch( Exception $ex ) {
+ 
+    // When validation fails or other local issues
+    echo $ex->message;
+  }
 }
-if ($session) {
-  // Logged in
-
-  echo 1111;
+ 
+// Check if a session exists
+if ( isset( $session ) ) {
+ 
+  // Save the session
+  $_SESSION['fb_token'] = $session->getToken();
+ 
+  // Create session using saved token or the new one we generated at login
+  $session = new FacebookSession( $session->getToken() );
+ 
+  // Create the logout URL (logout page should destroy the session)
+  $logoutURL = $helper->getLogoutUrl( $session, 'http://yourdomain.com/logout' );
+} else {
+  // No session
+ 
+  // Requested permissions - optional
+  $permissions = array(
+    'email',
+    'user_location',
+    'user_birthday'
+  );
+ 
+  // Get login URL
+  $loginUrl = $helper->getLoginUrl($permissions);
 }
 
 
+
+// Graph API to request user data
+$request = (new FacebookRequest( $session, 'GET', '/me' ))->execute();
+ 
+// Get response as an array
+$user = $request->getGraphObject()->asArray();
+
+// Graph API to request profile picture
+$request = (new FacebookRequest( $session, 'GET', '/me/picture?type=large&redirect=false' ))->execute();
+ 
+// Get response as an array
+$picture = $request->getGraphObject()->asArray();
 
 
 
