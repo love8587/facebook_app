bd274bdc1eda9ed1fff8958214cdb055dc120c31
Baek Byoeng han <bhbaek@Baeks-MacBook-Air.local>
2014-07-27 03:50:40 +0900

    as

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 require_once('autoload.php');
 
- 
 use Facebook\HttpClients\FacebookCurl;
 use Facebook\HttpClients\FacebookCurlHttpClient;
  
@@ -46,37 +45,35 @@ $helper = new FacebookPageTabHelper('518851781580229', '4284499c6fb57d117268cd20
 <div id="fb-root"></div>
 <script>
 
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      console.log(response.authResponse.accessToken);
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    }
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+  //console.log('statusChangeCallback');
+  //console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+    //console.log(response.authResponse.accessToken);
+    testAPI();
+  } else if (response.status === 'not_authorized') {
+    // The person is logged into Facebook, but not your app.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this app.';
+  } else {
+    // The person is not logged into Facebook, so we're not sure if
+    // they are logged into this app or not.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into Facebook.';
   }
+}       
 
-
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
-
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
 
  window.fbAsyncInit = function() {
       FB.init({
@@ -85,10 +82,6 @@ $helper = new FacebookPageTabHelper('518851781580229', '4284499c6fb57d117268cd20
         version    : 'v2.0'
       });
     };
-
-
-
-
 
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
