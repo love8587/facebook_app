f2fdeba151f2ac96f4ec4612ab0d3ea354d10513
Baek Byoeng han <bhbaek@Baeks-MacBook-Air.local>
2014-07-26 21:55:32 +0900

    sd

--- index.php
+++ index.php
@@ -1,56 +1,3 @@
-<?php 
-/*
-require_once('autoload.php');
-
- 
-use Facebook\HttpClients\FacebookCurl;
-use Facebook\HttpClients\FacebookCurlHttpClient;
- 
-use Facebook\Entities\AccessToken;
-use Facebook\Entities\SignedRequest;
- 
-use Facebook\FacebookSession;
-use Facebook\Helpers\FacebookRedirectLoginHelper;
-use Facebook\FacebookSignedRequestFromInputHelper; // added in v4.0.9
-use Facebook\FacebookRequest;
-use Facebook\FacebookResponse;
-use Facebook\FacebookSDKException;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookOtherException;
-use Facebook\FacebookAuthorizationException;
-use Facebook\GraphObject;
-use Facebook\GraphSessionInfo;
- 
-// these two classes required for canvas and tab apps
-use Facebook\Helpers\FacebookCanvasLoginHelper;
-use Facebook\Helpers\FacebookPageTabHelper;
- 
-// start session
-session_start();
- 
-// init app with app id and secret
-//FacebookSession::setDefaultApplication( 'xxx','yyy' );
-FacebookSession::setDefaultApplication('518851781580229','4284499c6fb57d117268cd20931f0ff5');
-
- 
- 
-$helper = new FacebookRedirectLoginHelper('http://boiling-caverns-1628.herokuapp.com/');
- 
-$session = $helper->getSessionFromRedirect();
- 
-if($session != NULL){
-    echo "Logged In !<br>";
-    $request = new FacebookRequest($session, 'GET', '/me');
-    $response = $request->execute();
-    $graph = $response->getGraphObject(GraphUser::className());
-    echo $graph->getName();
-}
-else{
-    echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
-}
-
-*/
-?>
 
 
 <!DOCTYPE html>
@@ -168,3 +115,66 @@ else{
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+<?php 
+
+require_once('autoload.php');
+
+ 
+use Facebook\HttpClients\FacebookCurl;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+ 
+use Facebook\Entities\AccessToken;
+use Facebook\Entities\SignedRequest;
+ 
+use Facebook\FacebookSession;
+use Facebook\Helpers\FacebookRedirectLoginHelper;
+use Facebook\FacebookSignedRequestFromInputHelper; // added in v4.0.9
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookOtherException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\GraphSessionInfo;
+ 
+// these two classes required for canvas and tab apps
+use Facebook\Helpers\FacebookCanvasLoginHelper;
+use Facebook\Helpers\FacebookPageTabHelper;
+ 
+// start session
+session_start();
+ 
+// init app with app id and secret
+//FacebookSession::setDefaultApplication( 'xxx','yyy' );
+FacebookSession::setDefaultApplication('518851781580229','4284499c6fb57d117268cd20931f0ff5');
+
+ 
+ 
+$helper = new FacebookRedirectLoginHelper('http://boiling-caverns-1628.herokuapp.com/');
+ 
+$session = $helper->getSessionFromRedirect();
+ 
+if($session != NULL){
+    echo "Logged In !<br>";
+    $request = new FacebookRequest($session, 'GET', '/me');
+    $response = $request->execute();
+    $graph = $response->getGraphObject(GraphUser::className());
+    echo $graph->getName();
+}
+else{
+    echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
+}
+
+
+?>
+
