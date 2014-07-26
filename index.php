4a7aac4b2d18de56b1a349711ee4ddf178fb74f3
Baek Byoeng han <bhbaek@Baeks-MacBook-Air.local>
2014-07-26 18:36:04 +0900

    a

--- index.php
+++ index.php
@@ -27,83 +27,32 @@ use Facebook\GraphObject;
 //FacebookSession::setDefaultApplication( 'APP_ID', 'APP_SECRET' );
 FacebookSession::setDefaultApplication('680605312014481','8f7df2bbfa7259bafe6ec9443f054776');
 
-// Create the login helper and replace REDIRECT_URI with your URL
-// Use the same domain you set for the apps 'App Domains'
-// e.g. $helper = new FacebookRedirectLoginHelper( 'http://mydomain.com/redirect' );
+
+// login helper with redirect_uri
 $helper = new FacebookRedirectLoginHelper( 'https://boiling-caverns-1628.herokuapp.com/' );
  
-// Check if existing session exists
-if ( isset( $_SESSION ) && isset( $_SESSION['fb_token'] ) ) {
- 
-  // Create new session from saved access_token
-  $session = new FacebookSession( $_SESSION['fb_token'] );
- 
-  // Validate the access_token to make sure it's still valid
-  try {
-    if ( ! $session->validate() ) {
-      $session = null;
-    }
-  } catch ( Exception $e ) {
-    // Catch any exceptions
-    $session = null;
-  }
-} else {
- 
-  // No session exists
-  try {
-    $session = $helper->getSessionFromRedirect();
-  } catch( FacebookRequestException $ex ) {
- 
-    // When Facebook returns an error
-  } catch( Exception $ex ) {
- 
-    // When validation fails or other local issues
-    echo $ex->message;
-  }
+try {
+  $session = $helper->getSessionFromRedirect();
+} catch( FacebookRequestException $ex ) {
+  // When Facebook returns an error
+} catch( Exception $ex ) {
+  // When validation fails or other local issues
 }
  
-// Check if a session exists
+// see if we have a session
 if ( isset( $session ) ) {
- 
-  // Save the session
-  $_SESSION['fb_token'] = $session->getToken();
- 
-  // Create session using saved token or the new one we generated at login
-  $session = new FacebookSession( $session->getToken() );
- 
-  // Create the logout URL (logout page should destroy the session)
-  $logoutURL = $helper->getLogoutUrl( $session, 'http://yourdomain.com/logout' );
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/me' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject();
+   
+  // print data
+  echo  print_r( $graphObject, 1 );
 } else {
-  // No session
- 
-  // Requested permissions - optional
-  $permissions = array(
-    'email',
-    'user_location',
-    'user_birthday'
-  );
- 
-  // Get login URL
-  $loginUrl = $helper->getLoginUrl($permissions);
+  // show login url
+  echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
 }
-
-
-
-// Graph API to request user data
-$request = (new FacebookRequest( $session, 'GET', '/me' ))->execute();
- 
-// Get response as an array
-$user = $request->getGraphObject()->asArray();
-
-// Graph API to request profile picture
-$request = (new FacebookRequest( $session, 'GET', '/me/picture?type=large&redirect=false' ))->execute();
- 
-// Get response as an array
-$picture = $request->getGraphObject()->asArray();
-
-
-
-
 
 
 
