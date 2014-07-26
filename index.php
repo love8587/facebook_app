7333e8ba18f68e08d254d62b6fbf2ed6d5f7e2d0
Baek Byoeng han <bhbaek@Baeks-MacBook-Air.local>
2014-07-26 15:57:54 +0900

    test

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php 
 
+require_once('autoload.php');
+
 
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
 use Facebook\GraphUser;
 use Facebook\FacebookRequestException;
 
-FacebookSession::setDefaultApplication('YOUR_APP_ID','YOUR_APP_SECRET');
+FacebookSession::setDefaultApplication('680605312014481','8f7df2bbfa7259bafe6ec9443f054776');
 
 // Use one of the helper classes to get a FacebookSession object.
 //   FacebookRedirectLoginHelper
 //   FacebookCanvasLoginHelper
 //   FacebookJavaScriptLoginHelper
 // or create a FacebookSession with a valid access token:
-$session = new FacebookSession('access-token-here');
+$session = new FacebookSession('83536eb622aa5a7038ef61199ccae1df');
 
 // Get the GraphUser object for the current user:
+
+var_dump($session);
 
 try {
   $me = (new FacebookRequest(
@@ -28,6 +32,18 @@ try {
   // Some other error occurred
 }
 
+$helper = new FacebookCanvasLoginHelper();
+try {
+  $session = $helper->getSession();
+var_dump($session);
+} catch(FacebookRequestException $ex) {
+  // When Facebook returns an error
+} catch(\Exception $ex) {
+  // When validation fails or other local issues
+}
+if ($session) {
+  // Logged in
+}
 
 
 ?>
